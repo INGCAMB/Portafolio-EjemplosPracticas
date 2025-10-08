245ad05a0688d873a90c79b1e9fa955712d8ac7b
Albertombn_0 <37099158+INGCAMB@users.noreply.github.com>
2025-10-08 12:51:30 -0700

    Add files via upload

---
+++ EjemplosWeb/EjemplosPHP/Ejemplo4/pagina1.php
@@ -0,0 +1,14 @@
+<HTML>
+	<HEAD>
+		<TITLE>Ejemplo 4 formulario de entrada del dato</TITLE>
+	</HEAD>
+	<BODY>
+		<FORM method="post" action="pagina2.php">
+			Ingrese su nombre:
+			<INPUT type="text" name="nombre">
+			<BR>
+			<INPUT type="submit"  value="confirmar">
+			<INPUT type="reset" value="borrar">
+		</FORM>
+	</BODY>
+</HTML>
---
+++ EjemplosWeb/EjemplosPHP/Ejemplo4/pagina2.php
@@ -0,0 +1,11 @@
+<HTML>
+	<HEAD>
+	<TITLE>Ejemplo 4 Captura de datos del form</TITLE>
+	</HEAD>
+	<BODY>
+		<?PHP
+			echo "El nombre ingresado es: ";
+			echo $_REQUEST['nombre'];
+		?>
+	</BODY>
+</HTML>
