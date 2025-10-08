d5333f293cbc64647ef5cd5e95c2e831d76059d2
Albertombn_0 <37099158+INGCAMB@users.noreply.github.com>
2025-10-08 12:43:03 -0700

    Crear ejemplo 1 de html con PHP - Ejemplo1.php

---
+++ EjemplosWeb/EjemplosPHP/Ejemplo1/Ejemplo1.php
@@ -0,0 +1,11 @@
+<HTML>
+	<HEAD>
+		<TITLE> Pagina1 </TITLE>
+	</HEAD>
+	<BODY> 
+		<H1>Ejemplo 1</H1>
+		<?php
+			echo "<SPAN style='color:red;'>Esto esta con php</SPAN>";
+		?>
+	</BODY>
+</HTML>
